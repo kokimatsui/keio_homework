b560e89799def55608b6b242308706c5293ab446
jockroiddev <jockroiddev@gmail.com>
2019-09-13 15:22:10 +0900

    modified

--- chr00.py
+++ chr00.py
@@ -5,7 +5,7 @@ import numpy as np
 
 #####データを読み取る#####
 artifical_dataset = lib.load( filename="artificial.csv" )
-explanatories = ["x1","const"]
+explanatories = ["const","x1"]
 explained = ["y"]
 
 #####各変数を定義#####
@@ -18,15 +18,15 @@ X_tX = np.dot( X.T , X )
 X_tY = np.dot( X.T , Y )
 b = np.dot( np.linalg.inv( X_tX ) , X_tY )
 
-print( "大問1.1の回答" )
+print( "<大問1.1の回答>" )
 lib.add_suffix( b )
 
-print( "大問1.2の回答" )
+print( "<大問1.2の回答>" )
 t = lib.t( X=X , Y=Y , beta=b )[0]
 print( "t value :" ,  )
 print( "自由度998で、2.581を基準にしたところ 2.581 <" , t , "なので、1%水準でβ0 = 0は棄却される" )
 
-print( "大問1.3の回答" )
+print( "<大問1.3の回答>" )
 artifical_dataset = lib.load( filename="artificial.csv" )
 explanatories = ["x1","x2","x3","const"]
 explained = ["y"]
--- chr02.py
+++ chr02.py
@@ -1,14 +1,35 @@
 #!-*-coding:utf-8-*-
 import lib
 import sys
+import numpy as np
 
-CEOSAL2_dataset = lib.load( filename="CEOSAL2.RAW" )
+CEOSAL2_dataset = lib.load( filename="CEOSAL2.csv" )
+explanatories = ["salary"]
+explained = ["lsalary"]
 
-print( "大問3.1の回答" )
+#####各変数を定義#####
+X = lib.df2mat( df=CEOSAL2_dataset , columns=explanatories )
+
+
+print( "<大問3.1の回答>" )
 #ここに回答プログラムを記載する
+print( "Salaryの平均値 :" , round( np.average( X ) , 3  ) )
 
-print( "大問3.2の回答" )
+print( "<大問3.2の回答>" )
 #ここに回答プログラムを記載する
+ceoten = CEOSAL2_dataset[ CEOSAL2_dataset["ceoten"] <=0  ]
+print( "CEOの在任期間が0の人の数 :", len( ceoten ) )
 
-print( "大問3.3の回答" )
+print( "<大問3.3の回答>" )
 #ここに回答プログラムを記載する
+CEOSAL2_dataset = lib.load( filename="CEOSAL2.csv" )
+explanatories = ["const","ceoten"]
+explained = ["lsalary"]
+
+X = lib.df2mat( df=CEOSAL2_dataset , columns=explanatories )
+Y = lib.df2mat( df=CEOSAL2_dataset , columns=explained )
+
+X_tX = np.dot( X.T , X )
+X_tY = np.dot( X.T , Y )
+b = np.dot( np.linalg.inv( X_tX ) , X_tY )
+lib.add_suffix( b )
