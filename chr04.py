e7e7c4e919b0722169f41a24b437fff305fe1d8c
jockroiddev <jockroiddev@gmail.com>
2019-09-14 13:22:29 +0900

    modified

--- chr04.py
+++ chr04.py
@@ -24,6 +24,7 @@ lib.add_suffix( b )
 t = lib.t( X=X , Y=Y , beta=b )[0]
 print( "t値 :", t )
 print( "1%有意で" , round( b[1] , 3  ) )
+print( "未回答" )
 
 print( "大問4.3の回答" )
 #ここに回答プログラムを記載する
@@ -33,10 +34,14 @@ explained = ["approve"]
 #####各変数を定義#####
 X = lib.df2mat( df=LOANAPP_dataset , columns=explanatories )
 Y = lib.df2mat( df=LOANAPP_dataset , columns=explained )
+
 X_tX = np.dot( X.T , X )
 X_tY = np.dot( X.T , Y )
 b = np.dot( np.linalg.inv( X_tX ) , X_tY )
+t = lib.t( X=X , Y=Y , beta=b )[0]
+print( "t値 :", t )
 lib.add_suffix( b )
+print( "依然として差別は存在する" )
 
 print( "大問4.4の回答" )
 #ここに回答プログラムを記載する
