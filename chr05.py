fcd4a45903c807978f95d20f1d4a71702caf8ac0
jockroiddev <jockroiddev@gmail.com>
2019-09-18 02:01:42 +0900

    modified

--- chr05.py
+++ chr05.py
@@ -7,4 +7,49 @@ import numpy as np
 """
 
 HPRICE_dataset = lib.load( filename="HPRICE1.csv" )
-print( HPRICE_dataset )
+explanatories = ["const","sqrft","bdrms"]
+explained = ["lprice"]
+
+#####各変数を定義#####
+X = lib.df2mat( df=HPRICE_dataset , columns=explanatories )
+Y = lib.df2mat( df=HPRICE_dataset , columns=explained )
+b = lib.reg( X=X , Y=Y )
+
+"""
+6-1の解答
+"""
+lib.add_suffix( b , labels=explanatories )
+
+
+"""
+6-2の解答
+"""
+
+"""
+6-3の解答
+"""
+
+
+"""
+6-4の解答
+"""
+
+"""
+6-5の解答
+"""
+
+"""
+6-6の解答
+"""
+
+"""
+6-7の解答
+"""
+
+"""
+6-8の解答
+"""
+
+"""
+6-9の解答
+"""
