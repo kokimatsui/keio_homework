f5489a79c5ed9b719422cd9fddb348ccc443d04b
jockroiddev <jockroiddev@gmail.com>
2019-09-18 18:09:04 +0900

    modified

---
+++ chr06.py
@@ -0,0 +1,38 @@
+#!-*-coding:utf-8-*-
+import lib
+import sys
+import numpy as np
+"""
+7 Panel Data
+"""
+print("#############7 Panel Data#############")
+
+"""
+7-1の解答
+"""
+print("<7.1の解答>")
+print("\n")
+
+"""
+7-2の解答
+"""
+print("<7.2の解答>")
+print("\n")
+
+"""
+7-3の解答
+"""
+print("<7.3の解答>")
+print("\n")
+
+"""
+7-4の解答
+"""
+print("<7.4の解答>")
+print("\n")
+
+"""
+7-5の解答
+"""
+print("<7.5の解答>")
+print("\n")
