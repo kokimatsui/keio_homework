e9b0f71fbde83345cc8c8ac477e8ab6ad6a1a815
jockroiddev <jockroiddev@gmail.com>
2019-09-18 16:02:12 +0900

    std_error added

--- lib/status.py
+++ lib/status.py
@@ -25,15 +25,22 @@ def var( X ):
     if X.ndim == 1:
         return np.var( X , ddof=1 )
 
+
 def t( X , Y , beta , beta_01=0 ):
     """
     t値
     """
+    st_err  = std_error( X=X , Y=Y , beta=beta )
+
+    return ( ( beta - beta_01 ) / st_err )
+
+def std_error( X , Y , beta ):
+
     X_bar = np.average( X )
     Y_hat = np.dot( X , beta )
-    std_error = np.sqrt( ( np.sum( (  Y - Y_hat  ) ** 2 ) / ( len( Y ) - 2 ) ) / np.sum( ( X - X_bar )**2 ) )
+    st_err = np.sqrt( ( np.sum( (  Y - Y_hat  ) ** 2 ) / ( len( Y ) - 2 ) ) / np.sum( ( X - X_bar )**2 ) )
 
-    return ( ( beta - beta_01 ) / std_error )
+    return st_err
 
 
 def t_level( free , level=5 ):
@@ -42,6 +49,9 @@ def t_level( free , level=5 ):
     """
 
     return t_test.ppf( q=( 1 - level * 0.01 ) , df=free )
+
+def conf_inter( beta , free , se ):
+    pass
 
 
 def _sampling( X ):
